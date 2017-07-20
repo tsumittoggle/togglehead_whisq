479afe8af8059f25c474cc329e891bc929f97784
Sumit <sumit@togglehead.in>
2017-07-20 15:20:43 +0530

    Adding locate us template file

---
+++ themes/whisq/template-parts/locate-us/bigbazar-address.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Whisq
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+  <h2>Bigbazar</h2>
+   <?php
+			$store = array( 
+				'post_type' => 'address',
+				'category_name' => 'bigbazar', 
+				'posts_per_page' => -1 );
+			$store_list = new WP_Query( $store );
+			while ( $store_list->have_posts() ) : $store_list->the_post();
+			?>
+          <h4 class="store-title"><?php the_title(); ?></h4>
+          <?php the_content(); ?>
+			<?php
+			endwhile;
+			wp_reset_query(); 
+	 ?>
