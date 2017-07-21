f3cd9811cc966e28c29435534e5af324ba19ed73
Sumit <sumit@togglehead.in>
2017-07-21 14:00:58 +0530

    locate us template added

--- themes/whisq/template-parts/locate-us/foodhall-address.php
+++ themes/whisq/template-parts/locate-us/foodhall-address.php
@@ -10,17 +10,70 @@
 
 ?>
 	<h2>Foodhall</h2>
-   <?php
-			$store = array( 
-				'post_type' => 'address',
-				'category_name' => 'foodhall', 
-				'posts_per_page' => -1 );
-			$store_list = new WP_Query( $store );
-			while ( $store_list->have_posts() ) : $store_list->the_post();
+      <?php
+    	global $wpdb;
+			$tags = get_terms('post_tag');
 			?>
-          <h4 class="store-title"><?php the_title(); ?></h4>
-          <?php the_content(); ?>
-			<?php
-			endwhile;
-			wp_reset_query(); 
+			  <div class="select-section">
+			  <form action="#" name="cityselect" method="post">
+    	    <select name="select-city" id="cities" class="cities" onchange="this.form.submit()">
+    	    		<option name"city" value="city">city</option>
+							<?php
+							foreach ($tags as $tag){
+							?>
+								<option name="<?php echo $tag->name; ?>" value="<?php echo $tag->name; ?>"><?php echo $tag->name; ?></option>
+							<?php
+							}
+              ?>
+          </select>
+          </form>
+        </div>     
+				<?php
+				  $selected_val = 'city';
+				  if(isset($_POST['select-city'])){
+						$selected_val = $_POST['select-city'];  
+						echo "You have selected city:" .$selected_val;  
+				  }
+				?>
+    <?php
+      if($selected_val == "city" || $selected_val == " ") {
+			foreach ($tags as $tag){
+				?>
+				 <div class="city-name"><?php echo $tag->name; ?></div> 
+				  <?php
+						$store = array( 
+							'post_type' => 'address',
+							'category_name' => 'foodhall', 
+							'posts_per_page' => -1,
+							'tag' => $tag->name
+							);
+						$store_list = new WP_Query( $store );
+						while ( $store_list->have_posts() ) : $store_list->the_post();
+						?>
+			          <h4 class="store-title"><?php the_title(); ?></h4>
+			          <?php the_content(); ?>
+						<?php
+						endwhile;
+						wp_reset_query(); 
+					}
+				}
+				else {
+				?>
+				 <div class="city-name"><?php echo $selected_val; ?></div> 
+				  <?php
+						$store = array( 
+							'post_type' => 'address',
+							'category_name' => 'foodhall', 
+							'posts_per_page' => -1,
+							'tag' => $selected_val
+							);
+						$store_list = new WP_Query( $store );
+						while ( $store_list->have_posts() ) : $store_list->the_post();
+						?>
+			          <h4 class="store-title"><?php the_title(); ?></h4>
+			          <?php the_content(); ?>
+						<?php
+						endwhile;
+						wp_reset_query(); 					
+				}
 	 ?>
