fd8e5f7624fb244ecf4043e781b380461675ec08
Sumit <sumit@togglehead.in>
2017-07-26 11:00:27 +0530

    header icon problem resolved

--- themes/whisq/template-parts/header/header-top.php
+++ themes/whisq/template-parts/header/header-top.php
@@ -10,9 +10,18 @@
 
 ?>
 <div class="header-top">
-   <a class="user-btn" title="User"><img src="<?php the_field('user'); ?>" class="fa ffa-user"/></a>
+   <?php if ( is_front_page() ) {?> 
+   <a class="user-btn" title="User"><img src="wp-content/uploads/user-icon.png" class="fa ffa-user"/></a>
     <div class="user-profile">
    		<?php //echo do_shortcode('[woocommerce_my_account current_user]'); ?>
    </div>
-   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="<?php the_field('shopping'); ?>" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
+   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="wp-content/uploads/shopping-cart.png" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
+   <?php } 
+   else {?>
+     <a class="user-btn" title="User"><img src="../wp-content/uploads/user-icon.png" class="fa ffa-user"/></a>
+    <div class="user-profile">
+   		<?php //echo do_shortcode('[woocommerce_my_account current_user]'); ?>
+   </div>
+   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="../wp-content/uploads/shopping-cart.png" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
+   <?php } ?>
 </div><!-- .custom-header -->
