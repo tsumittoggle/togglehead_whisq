62f2e7327fde82b935278a9bf7c10f6f878c4db3
Sumit <sumit@togglehead.in>
2017-07-28 17:03:58 +0530

    header icon problem resolved

--- themes/whisq/template-parts/header/header-top.php
+++ themes/whisq/template-parts/header/header-top.php
@@ -10,18 +10,10 @@
 
 ?>
 <div class="header-top">
-   <?php if ( is_front_page() ) {?> 
-   <a class="user-btn" title="User"><img src="wp-content/uploads/user-icon.png" class="fa ffa-user"/></a>
+   <a class="user-btn" title="User"><img src="<?php echo esc_url( home_url( '/wp-content/uploads/user-icon.png') ); ?>" class="fa ffa-user"/></a>
     <div class="user-profile">
    		<?php //echo do_shortcode('[woocommerce_my_account current_user]'); ?>
    </div>
-   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="wp-content/uploads/shopping-cart.png" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
-   <?php } 
-   else {?>
-     <a class="user-btn" title="User"><img src="../wp-content/uploads/user-icon.png" class="fa ffa-user"/></a>
-    <div class="user-profile">
-   		<?php //echo do_shortcode('[woocommerce_my_account current_user]'); ?>
-   </div>
-   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="../wp-content/uploads/shopping-cart.png" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
-   <?php } ?>
+   <a class="cart-customlocation shop-cart" href="<?php echo wc_get_cart_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"><img src="<?php echo esc_url( home_url( '/wp-content/uploads/shopping-cart.png') ); ?>" class="fa ffa-shopping-cart"/><span class="count"><?php echo sprintf ( _n( '%d', '%d', WC()->cart->get_cart_contents_count() ), WC()->cart->get_cart_contents_count() ); ?></span></a>
+  
 </div><!-- .custom-header -->
