1a7621f469dd614c27f980974d782e83645faddc
Sumit <sumit@togglehead.in>
2017-07-31 15:25:32 +0530

    recipe detail page adding

--- themes/whisq/single.php
+++ themes/whisq/single.php
@@ -2,10 +2,9 @@
 /**
  * The template for displaying all single posts
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package WordPress
- * @subpackage Twenty_Seventeen
+ * @subpackage Whisq
  * @since 1.0
  * @version 1.0
  */
@@ -19,25 +18,14 @@ get_header(); ?>
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/post/content', get_post_format() );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'whisq' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'whisq' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . whisq_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'whisq' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'whisq' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . whisq_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
-
+			if ( post_type_exists( 'recipes' ) ) {
+                get_template_part( 'template-parts/post/recipes', 'content' ); 
+            }
 			endwhile; // End of the loop.
 			?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
 </div><!-- .wrap -->
 
 <?php get_footer();
---
+++ themes/whisq/template-parts/post/recipes-content.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Whisq
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+	<div class="whisqtitle">
+		<h2>recipes</h2>
+		<p class="wrapper breadcrumb-url"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">Home</a> > <a href="<?php echo esc_url( home_url( '/recipes/' ) ); ?>" rel="home">Recipes</a> > <span><?php the_title(); ?></span></p>
+	</div>
+<div class="header-post">
+<?php the_post_thumbnail(); ?>
+</div>	
+<div class="wrapper">
+	<h2 class="post-title"><?php the_title(); ?></h2>
+	<div class="list">
+		<?php if(get_field('servings')) : ?>
+			<span><?php the_field('servings'); ?></span>
+		<?php endif; ?>
+		<?php if(get_field('preparation')) : ?>
+			<span><?php the_field('preparation'); ?></span>
+		<?php endif; ?>
+		<?php if(get_field('baking')) : ?>
+			<span><?php the_field('baking'); ?></span>
+		<?php endif; ?>
+		<?php if(get_field('email_recipes')) : ?>
+			<span><?php the_field('email_recipes'); ?></span>
+		<?php endif; ?>
+	</div>
+	<p><?php the_excerpt(); ?></p>
+	<div class="recipe-detail">
+	  <?php if(get_field('integedien')) : ?>
+		<div class="ingrediean">
+			<h4>ingrediean</h4>
+			<div class="ingrediean-content">
+				<?php the_field('integedien'); ?>
+			</div>
+		</div>
+	<?php endif; ?>
+	<?php if(get_field('method')) : ?>
+		<div class="method">
+			<h4>method</h4>
+			<div class="method-content">
+			  <?php the_field('method'); ?>
+			</div>
+		</div>
+	<?php endif; ?>
+	</div>
+  <div class="share">
+  	<span>share</span>
+  	<?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
+  </div>
+  <div class="tag">
+  	<?php if(get_field('tags')) : ?>
+			<p>tags</p><?php the_field('tags'); ?>
+		<?php endif; ?>
+  </div>
+  <div class="post-change">
+	  <span class="prev"><?php previous_post_link(); ?></span>
+	  <span class="next"><?php next_post_link(); ?></span>     
+  </div>
+</div>
+<div class="extra-wrapper">
+  <ul class="select-extra">
+  	<li>you may also like</li>
+  	<li>accesories used for make recipe</li>
+  </ul>
+	<div class="recipe cf">
+  <?php
+		$recipe = array( 'post_type' => 'recipes', 'posts_per_page' => 3, 'orderby' => 'rand' );
+		$recipe_list = new WP_Query( $recipe );
+		while ( $recipe_list->have_posts() ) : $recipe_list->the_post();
+		?>
+			<div class="recipes-page-list">
+			  <div class="recipe-page-img">
+			  	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			  		<?php the_post_thumbnail(); ?>
+			  	</a>
+			  </div>		  
+			  <h3>
+          <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+				    <?php the_title(); ?>
+				  </a>
+				</h3>
+				<p><?php the_excerpt(); ?></p>
+				<div class="share">
+        	<span>share</span>
+  	      <?php echo do_shortcode('[DISPLAY_ULTIMATE_SOCIAL_ICONS]'); ?>
+       </div>
+			</div>
+		</div>
+		<?php
+		endwhile;
+		wp_reset_query(); 
+  ?>
+  <div class="related-accesories">
+  			<?php
+		  
+			$bandproduct_args = array(
+				'post_type'           => 'product',
+				'product_cat'         =>  '',
+				'post_status'         => 'publish',
+				'posts_per_page'      => '3',
+				'orderby'             =>  'rand',
+				'order'               => 'ASC',
+			);
+			$loop = new WP_Query( $bandproduct_args );
+			    while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+			    
+			       <div class="product-list">  
+			          <div class="img-box">  
+			          	<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			            <?php 
+			                if ( has_post_thumbnail( $loop->post->ID ) )
+			                    echo get_the_post_thumbnail( $loop->post->ID, 'shop_catalog' ); 
+			                else 
+			                    echo '<img src="' . woocommerce_placeholder_img_src() . '" alt="Placeholder" />'; 
+			            ?>
+			            </a>
+			            </div>
+			            <a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+			            	<h3 class="product-title"><?php the_title(); ?></h3>
+			            </a>
+
+			            <?php 
+			                echo $product->get_price_html(); 
+			                woocommerce_template_loop_add_to_cart( $loop->post, $product );
+			            ?>    
+			        </div>
+
+			<?php 
+			    endwhile;
+			    wp_reset_query(); 
+			?>
+  </div>
+</div>
+	    
