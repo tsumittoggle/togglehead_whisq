ebb612df52f65541c4b49626641c66141c364c0f
Dharmesh <dharmesh@togglehead.in>
2017-08-16 07:26:46 +0530

    changes to event page and details

--- themes/whisq/assets/js/script.js
+++ themes/whisq/assets/js/script.js
@@ -260,9 +260,24 @@ $('.sort-cat li').click(function(){
   });
 
 
+//zoom error creating problem for media query
+$(document).keydown(function(event) {
+if (event.ctrlKey==true && (event.which == '61' || event.which == '107' || event.which == '173' || event.which == '109'  || event.which == '187'  || event.which == '189'  ) ) {
+        event.preventDefault();
+     }
+    // 107 Num Key  +
+    // 109 Num Key  -
+    // 173 Min Key  hyphen/underscor Hey
+    // 61 Plus key  +/= key
+});
 
-
- 	
+$(window).bind('mousewheel DOMMouseScroll', function (event) {
+       if (event.ctrlKey == true) {
+       event.preventDefault();
+       }
+});
+ //zoom error closed
+ 
 	$("#all").trigger("click");
 	
 	
@@ -347,7 +362,7 @@ if(window.innerWidth > 768 ){
      maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
    });
 
-   $('.event-excerpt p').each(function() {
+   $('.event-excerpt p:first-child').each(function() {
      $(this).height(maxHeight);
    });
 		}		
--- themes/whisq/tribe-events/single-event.php
+++ themes/whisq/tribe-events/single-event.php
@@ -54,9 +54,8 @@ if($today_time <= $expire_time) {
       <?php echo tribe_get_full_address ($post->ID, false); ?>
     </address>
 	</div>
-  <div class="single-event-content">
+  <div class="single-event-content cf">
 	  <div class="event-left">
-	  	<?php the_title( '<h2>', '</h2>' ); ?>
 	  	<?php the_content(); ?>
 	  	<script type="text/javascript">
 		 (function () {
@@ -88,48 +87,26 @@ if($today_time <= $expire_time) {
   </div>	
   <?php } else { ?>
 	<div class="detail-img">
+	<p class="event-day">
+      <span><?php echo tribe_get_start_date( $post->ID, false, 'M' ); ?></span>
+      <span><?php echo tribe_get_start_date( $post->ID, false, 'd' ); ?></span>
+    </p>
 		<img src=" <?php echo get_the_post_thumbnail_url(); ?>">
 	</div>
-	<div class="event-date-time">
-	  <div class="event-day">
-      <span><?php echo tribe_get_start_date( $post->ID, false, 'M' ); ?></span>
-      <span><?php echo tribe_get_start_date( $post->ID, false, 'Y' ); ?></span>
-    </div>
+	<div class="event-date-time cf">
+	  
     <div class="event-time">
-      <span><?php echo tribe_get_start_date( $post->ID, false, 'j a' ); ?></span>
-      <span><?php echo tribe_get_end_date( $post->ID, false, 'j a' ); ?></span>
+	
+      <span class="eventtimespan"><img src="<?php echo esc_url( home_url( '/wp-content/uploads/time.png') ); ?>" class="singleeventico"><?php echo tribe_get_start_date( $post->ID, false, 'j a' ); ?> - <?php echo tribe_get_end_date( $post->ID, false, 'j a' ); ?></span>
     </div>
     <address>
+	<img src="<?php echo esc_url( home_url( '/wp-content/uploads/location.png') ); ?>" class="singleeventico">
       <?php echo tribe_get_full_address ($post->ID, false); ?>
     </address>
 	</div>
   <div class="single-event-content">
-	  <div class="event-left">
-	  	<?php the_title( '<h2>', '</h2>' ); ?>
+	  <div class="pastevent-left">
 	  	<?php the_content(); ?>
-	  	<script type="text/javascript">
-		 (function () {
-            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
-                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-                var h = d[g]('body')[0];h.appendChild(s); }})();
-    </script>
-
-    <!-- 3. Place event data -->
-    <span class="addtocalendar atc-style-blue">
-        <var class="atc_event">
-            <var class="atc_date_start">2015-05-04 12:00:00</var>
-            <var class="atc_date_end">2015-05-04 18:00:00</var>
-            <var class="atc_timezone">Europe/London</var>
-            <var class="atc_title">Star Wars Day Party</var>
-            <var class="atc_description">May the force be with you</var>
-            <var class="atc_location">Tatooine</var>
-            <var class="atc_organizer">Luke Skywalker</var>
-            <var class="atc_organizer_email">luke@starwars.com</var>
-        </var>
-    </span>
 	  </div>
 	  <div class="event-form">
 	  <?php
@@ -148,6 +125,10 @@ if($today_time <= $expire_time) {
 			    </ul>
 			<?php endif; ?>
 	  </div>
+	  <div class="share">
+       <span>Share</span>
+       <?php echo do_shortcode('[addtoany]'); ?>
+       </div>
   <?php	}?>
 </div>	
 
