923a8958207570fc985a048f7f3a608a64a18f93
santosh <santosh@togglehead.in>
2017-08-17 18:59:50 +0530

    contact front page with content completed

--- themes/whisq/contact_page.php
+++ themes/whisq/contact_page.php
@@ -4,7 +4,45 @@
 */
 get_header();
  ?>
+<div class="container contact-page">
+	<div class="col-md-6 left">
+	   				<?php if(get_field('content') ) : ?>
+       	           <?php the_field('content'); ?>
+                <?php endif; ?>
+	</div>
+	<div class="col-md-6 right">
+       <?php if(get_field('form_code') ) : ?>
+       	 <?php the_field('form_code'); ?>
+       <?php endif; ?>
+	</div>
+</div>
 
+<section class="below-section">
+	<div class="container">
+	<div class="col-md-6">
+		<div class="wrapper">
+			<div class="img-part">
+				<img src="http://via.placeholder.com/30x30">
+			</div>
+			<div class="para-part">
+				<?php if(get_field('email') ) : ?>
+       	           <?php the_field('email'); ?>
+                <?php endif; ?>
+			</div>
+		</div>
+		<div class="wrapper">
+			<div class="img-part">
+				<img src="http://via.placeholder.com/30x30">
+			</div>
+			<div class="para-part">
+		       <?php if(get_field('address') ) : ?>
+		       	 <?php the_field('address'); ?>
+		       <?php endif; ?>
+			</div>
+		</div>
+	</div>		
+	</div>
+</section>
 <?php
 get_footer();
 ?>
